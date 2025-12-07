fd933f8fb19de5cc3a82be0cf34a0e4eb67a01ae
Vethariel <dagraciap@unal.edu.co>
2025-12-06 21:03:19 -0500

    Printing info from proc meminfo

--- test_1_1.c
+++ test_1_1.c
@@ -1,6 +1,17 @@
 # include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    FILE *fp;
+    char buffer[255];
+
+    fp = fopen("/proc/meminfo", "r");
+    if (fp == NULL) {
+        perror("Error opening file");
+        return -1;
+    }
+    while(fgets(buffer, sizeof(buffer), fp) != NULL) {
+        printf("%s", buffer);
+    }
+    fclose(fp);
     return 0;
 }
